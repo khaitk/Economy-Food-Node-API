6492f1198ce4c1a0214bea85ba79754eb0b5721c
Khai Dev <khaitkdev@gmail.com>
2022-10-01 13:47:44 +0700

    initial project

---
+++ src/api/v1/routes/routes.js
@@ -0,0 +1,14 @@
+//import controllers
+const testController = require('../controllers/TestController')
+
+module.exports = function ( app ) {
+    //khai báo router
+    app.get('/', (req, res) => {
+        res.send({message : 'Khai TK - Node JS - MySQL - Docker'})
+    })
+
+    app.get('/get-test', testController.getTest )
+    //app.get( '/get-member/:memberCode', memberController.getMember )
+    //app.post( '/sign-in', validate.validateLogin(), memberController.logIn )
+
+};
